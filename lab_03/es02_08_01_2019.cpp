3663713f6fa7ffa704f643ea23e4953ada44f9db
Damiano Zanardo <damiano.zanardo@gmail.com>
2019-01-12 13:29:34 +0100

    update

--- lab_03/es02_08_01_2019.cpp
+++ lab_03/es02_08_01_2019.cpp
@@ -1,14 +1,44 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main()
-{
+int main() {
     // delimitatore iniziale output del programma (stringa "start")
     // ATTENZIONE! Non cancellare l'istruzione sottostante
     cout << "start" << endl;
-    
+
     // inserire qui il codice del programma
-    
+    int n;
+    cin >> n;
+    if (n >= 3) {
+        int a = 0;
+        while (a < (n - 2)) {
+            cout << " ";
+            a++;
+        }
+        cout << "*" << endl;
+
+        int n_ = 0;
+        while (n_ < n - 1) {
+            a = 0;
+            while (a < n_) {
+                cout << " ";
+                a++;
+            }
+            a = 0;
+            while (a < (n - n_) * 2 - 3) {
+                cout << "*";
+                a++;
+            }
+
+            cout << endl;
+            n_++;
+        }
+
+    } else {
+        cout << "Altezza non valida" << endl;
+    }
+
     // delimitatore finale output del programma (stringa "end")
     // ATTENZIONE! Non cancellare l'istruzione sottostante
     cout << "end" << endl;
--- lab_03/es03_08_01_2019.cpp
+++ lab_03/es03_08_01_2019.cpp
@@ -1,14 +1,59 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main()
-{
+int main() {
     // delimitatore iniziale output del programma (stringa "start")
     // ATTENZIONE! Non cancellare l'istruzione sottostante
     cout << "start" << endl;
-    
+
     // inserire qui il codice del programma
-    
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    int dx = (x2 - x1);
+    int dy = (y2 - y1);
+
+    if (dx < 0) {
+        dx = -dx;
+    }
+    if (dy < 0) {
+        dy = -dy;
+    }
+
+    if (dx == dy) {
+        if (y1 > y2 && x2 > x1) {
+            int dx_ = 0;
+            int a = 0;
+            while (dx_ < dx) {
+                a = 0;
+                while (a < dx_) {
+                    cout << " ";
+                    a++;
+                }
+
+                cout << "*" << endl;
+                dx_++;
+            }
+        } else {
+            int dx_ = dx;
+            int a;
+            while (dx_ > 0) {
+                a = 1;
+                while (a < dx_) {
+                    cout << " ";
+                    a++;
+                }
+
+                cout << "*" << endl;
+                dx_--;
+            }
+        }
+
+    } else {
+        cout << "Le coordinate immesse non formano la diagonale di un quadrato" << endl;
+    }
+
     // delimitatore finale output del programma (stringa "end")
     // ATTENZIONE! Non cancellare l'istruzione sottostante
     cout << "end" << endl;
