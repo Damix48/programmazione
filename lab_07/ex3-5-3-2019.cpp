152e8482f1936d62f89839dcfd4daf7e8ebac3c8
Damiano Zanardo <damiano.zanardo@gmail.com>
2019-03-05 10:08:07 +0100

    Update ex3-5-3-2019.cpp

--- lab_07/ex3-5-3-2019.cpp
+++ lab_07/ex3-5-3-2019.cpp
@@ -10,6 +10,30 @@ main()
   }
   cout << "start" << endl;
   //da fare
+  int i = 0;
+  while (i < N)
+  {
+    if (A[i] == 1)
+    {
+      int j = i + 1;
+      while (j < N)
+      {
+        if (A[j] == 0)
+        {
+          int temp = A[i];
+          A[i] = A[j];
+          A[j] = temp;
+        }
+        j++;
+      }
+    }
+    i++;
+  }
+
+  for (int i = 0; i < N; i++)
+  {
+    cout << A[i] << " ";
+  }
 
   cout << "end" << endl;
 }
