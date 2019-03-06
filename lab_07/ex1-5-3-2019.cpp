adfb7cd1b2581f519ec5969e08d403a1a841eb66
Damiano Zanardo <damiano.zanardo@gmail.com>
2019-03-06 18:18:30 +0100

    update

--- lab_07/ex1-5-3-2019.cpp
+++ lab_07/ex1-5-3-2019.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
 using namespace std;
-main()
-{
-  int N, A[100], ind1, ind2;
-  bool palindromo = true;
-  cin >> N;
-  for (int i = 0; i < N; i++)
-  {
-    cin >> A[i];
-  }
-  cout << "start" << endl;
 
-  for (int i = 0; i < N / 2 && palindromo; i++)
-  {
-    if (A[i] != A[N - 1 - i])
-    {
-      palindromo = false;
-      ind1 = i;
-      ind2 = N - 1 - i;
+int main() {
+    int N, A[100], ind1, ind2;
+    bool palindromo = true;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
     }
-  }
+    cout << "start" << endl;
 
-  if (palindromo)
-  {
-    cout << "A è un palindromo" << endl;
-  }
-  else
-  {
-    cout << "A non è un palindromo e la prima contraddizione riguarda gli elementi di indice " << ind1 << " e " << ind2 << endl;
-  }
+    for (int i = 0; i < N / 2 && palindromo; i++) {
+        if (A[i] != A[N - 1 - i]) {
+            palindromo = false;
+            ind1 = i;
+            ind2 = N - 1 - i;
+        }
+    }
 
-  cout << "end" << endl;
+    if (palindromo) {
+        cout << "A è un palindromo" << endl;
+    } else {
+        cout << "A non è un palindromo e la prima contraddizione riguarda gli "
+                "elementi di indice "
+             << ind1 << " e " << ind2 << endl;
+    }
+
+    cout << "end" << endl;
 }
--- lab_07/ex3-5-3-2019.cpp
+++ lab_07/ex3-5-3-2019.cpp
@@ -1,39 +1,33 @@
 #include <iostream>
 using namespace std;
-main()
-{
-  int N, A[100];
-  cin >> N;
-  for (int i = 0; i < N; i++)
-  {
-    cin >> A[i];
-  }
-  cout << "start" << endl;
-  //da fare
-  int i = 0;
-  while (i < N)
-  {
-    if (A[i] == 1)
-    {
-      int j = i + 1;
-      while (j < N)
-      {
-        if (A[j] == 0)
-        {
-          int temp = A[i];
-          A[i] = A[j];
-          A[j] = temp;
-        }
-        j++;
-      }
+
+int main() {
+    int N, A[100];
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
     }
-    i++;
-  }
+    cout << "start" << endl;
+    // da fare
+    int i = 0;
+    while (i < N) {
+        if (A[i] == 1) {
+            int j = i + 1;
+            while (j < N) {
+                if (A[j] == 0) {
+                    int temp = A[i];
+                    A[i] = A[j];
+                    A[j] = temp;
+                }
+                j++;
+            }
+        }
+        i++;
+    }
 
-  for (int i = 0; i < N; i++)
-  {
-    cout << A[i] << " ";
-  }
+    for (int i = 0; i < N; i++) {
+        cout << A[i] << " ";
+    }
 
-  cout << "end" << endl;
+    cout << "end" << endl;
 }
