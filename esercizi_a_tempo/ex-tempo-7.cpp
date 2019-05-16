3085181ace9ce47d135b27897c8b19dc3eeefb60
Damix48 <damiano.zanardo@gmail.com>
2019-05-16 18:28:01 +0200

    up

--- esercizi_a_tempo/ex-tempo-7.cpp
+++ esercizi_a_tempo/ex-tempo-7.cpp
@@ -3,109 +3,104 @@ using namespace std;
 
 struct nodo {
     int info;
-    nodo *next;
-    nodo(int a = 0, nodo *b = 0) {
-        info = a;
-        next = b;
+    nodo *left;
+    nodo *right;
+
+    nodo(int x = 0, nodo *l = 0, nodo *r = 0) {
+        info = x;
+        left = l;
+        right = r;
     }
 };
 
-struct nodoP {
-    nodo *P;
-    nodoP *next;
-    nodoP(nodo *a = 0, nodoP *b = 0) {
-        P = a;
-        next = b;
-    }
-};
+// FUNZIONI AUSILIARIE FORNITE
+nodo *buildTree(int arr[], int i, int n) {
 
-// PRE=(cin contiene n valori sequiti da -1 con n>=0)
-nodo *leggi() {
-    int temp;
-    cin >> temp;
-    if (temp == -1) {
+    if (i >= n) {
+        return NULL;
+    }
+
+    nodo *root = new nodo(arr[i]);
+    // insert left child
+    root->left = buildTree(arr, 2 * i + 1, n);
+    // insert right child
+    root->right = buildTree(arr, 2 * i + 2, n);
+
+    return root;
+}
+
+int height(nodo *root) {
+    if (!root) {
         return 0;
     }
-    return new nodo(temp, leggi());
-}
-// POST=(restituisce una lista concatenata con n nodi tale che il campo info del
-// nodo 0 sia x0, quello del nodo 1 sia x1, e così via)
-
-void stampa(nodoP *temp) {
-    if (temp) {
-        cout << temp->P->info << " ";
-        stampa(temp->next);
-    }
+    return 1 + max(height(root->left), height(root->right));
 }
 
-// PRE=(Lista(L) è ben formata, x è definito)
-nodoP *aux(nodo *L, int x) {
-    if (!L) {
-        return 0;
-    }
-    if (L->info != x) {
-        return aux(L->next, x); //(a)
-    }
-    return new nodoP(L, aux(L->next, x)); //(b)
-}
-// POST=(restituisce una lista di nodoP lunga come il n. di nodi di Lista(L)
-// con x=info e i cui nodi puntano a questi nodi di L nell’ordine in cui i
-// nodi sono in L)
-
-/*   CORRETTEZZA   */
-
-// - caso base (!L): non esiste il nodo L nella lista Lista(L), la funzione
-// restituisce quindi 0.
-
-// - caso ricorsivo: L->info==x trova un match nel primo nodo di L e creo un
-// nodo che punta ad L tramite il campo info della lista creata sulla base di
-// nodoP
-// aux(L->next, x) sia in (a) e in (b) invoca la chiamata ricorsiva per
-// controllare eventuali match di x con altri nodi della lista L
-
-//       PRE_ric: Lista(L) è ben formata && x è definito; è vero, e quindi vale
-//       la POST_ric cioè dopo
-
-//               if (L->info != x) {
-//                       return aux(L->next, x);
-//               }
-//               return new nodoP(L, aux(L->next,x));
-
-//       la funzione ha restituito una lista di nodoP lunga come il numero di
-//       nodi di Lista(L) con info==x e i cui nodi puntano a questi nodi fi L
-//       nell'ordine in cui i nodi sono in L.
-
-nodoP *ultimo(nodoP *y) {
-    if (!y->next) {
-        return y;
-    }
-    return ultimo(y->next);
-}
-
-nodoP *F(nodo *L) {
-    int temp;
-    cin >> temp;
-    if (temp == -1) {
-        return 0;
-    }
-
-    nodoP *y = aux(L, temp);
-    // nodoP *y = ultimo(aux(L, temp));
-    // stampa(y);
-    if (!y) {
-        return F(L);
+void stampa_l(nodo *r) {
+    if (r) {
+        cout << r->info << '(';
+        stampa_l(r->left);
+        cout << ',';
+        stampa_l(r->right);
+        cout << ')';
     } else {
-        ultimo(y)->next = F(L);
+        cout << '_';
     }
-
-    return y;
 }
+
+void sc(int *C) {
+    if (*C != -1) {
+        cout << *C << ' ';
+        sc(C + 1);
+    } else {
+        cout << "fine cammino" << endl;
+    }
+}
+// PRE=(albero(r) è ben formato e non vuoto, k>=0 e y valore qualsiasi, C ha
+// almeno tanti elementi quanta è l'altezza di albero(r))
+bool cerca_cam(nodo *r, int k, int y, int *C) {
+    if (k == 0) {
+        return false;
+    }
+    if (k == 1) {
+    }
+    if (r->left || r->right) {
+        if (r->info == y) {
+            k--;
+            return (r->left)
+        }
+    }
+}
+// POST_cerca = (restituisce true sse in albero(r) esiste un cammino da r ad una
+// foglia con esattamente k nodi con campo info=y e false altrimenti) && (in
+// caso restituisca true, C contiene una sequenza (anche vuota) di 0/1 seguita
+// da -1 e che individua il cammino più a sinistra in albero(r) con esattamente
+// k nodi con campo info=y).
 
 int main() {
+    int dim;
+    cin >> dim;
+    int *a = new int[dim];
+    for (int i = 0; i < dim; i++) {
+        cin >> a[i];
+    }
+
+    nodo *root = NULL;
+    root = buildTree(a, 0, dim);
+    int h = height(root);
+    int *C = new int[h];
+
+    int k, y;
+    cin >> k >> y;
     cout << "start" << endl;
-    nodo *L = leggi();
-    nodoP *y = F(L);
-    stampa(y);
+    bool b = cerca_cam(root, k, y, C);
     cout << endl;
+    if (b) {
+        cout << "trovato  cammino=";
+        sc(C);
+    } else {
+        cout << " nessun cammino con " << k << " valori=" << y << endl;
+    }
+    cout << "end";
     cout << "end" << endl;
 }
