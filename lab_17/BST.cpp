e9eed5b9ba257016a891a0c19039789c00af9d2f
Damiano Zanardo <damiano.zanardo@gmail.com>
2019-05-28 09:48:23 +0200

    Add files via upload

---
+++ lab_17/BST.cpp
@@ -0,0 +1,84 @@
+#include "BST.h"
+#include <iostream>
+using namespace std;
+// contiene le implementazioni delle 7 funzioni richieste
+
+void stampa_l(nodo *r) {
+    if (r) {
+        cout << r->info;
+        cout << "(";
+        stampa_l(r->left);
+        cout << ",";
+        stampa_l(r->right);
+        cout << ")";
+    } else {
+        cout << "_";
+    }
+}
+
+nodo *insert(nodo *r, int x) {
+    if (!r) {
+        return new nodo(x);
+    }
+    if (x < r->info) {
+        r->left = insert(r->left, x);
+    } else {
+        r->right = insert(r->right, x);
+    }
+    return r;
+}
+
+bool search(nodo *r, int x){
+    if(!r){
+        return false;
+    }
+    if(r->info==x){
+        return true;
+    }
+    if(x<r->info){
+        return search(r->left,x);
+    }else{
+        return search(r->right,x);
+    }
+}
+nodo *max(nodo *r){
+    if(!r->right){
+        return r;
+    }else{
+        return max(r->right);
+    }
+}
+nodo *min(nodo *r){
+    if(!r->left){
+        return r;
+    }else{
+        return min(r->left);
+    }
+}
+
+int altezza(nodo *r){
+    if(!r){
+        return -1;
+    }
+    int a=altezza(r->left)+1;
+    int b=altezza(r->right)+1;
+
+    if(a>b){
+        return a;
+    }
+    return b;
+}
+
+int altMin(nodo *r){
+    if(!r){
+        return 0;
+    }
+
+    int a=altMin(r->left)+1;
+    int b=altMin(r->right)+1;
+
+    if(a<b){
+        return a;
+    }
+    return b;
+}
